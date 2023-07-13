f4afc502a8e4d109354833b974fc28f0a2317497
Thomas Sleeman <60188208+thomassleeman@users.noreply.github.com>
2023-07-13 14:05:58 +0100

    Updated middleware function get request to fetch correct domain not localhost

--- middleware.ts
+++ middleware.ts
@@ -1,38 +1,32 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { type } from "os";
 
 export async function middleware(request: NextRequest, response: NextResponse) {
-  const session = request.cookies.get('session');
-
-  // if (request.nextUrl.pathname.startsWith('/signup')) {
-  //   return NextResponse.rewrite(new URL('/about-2', request.url));
-  // }
+  const session = request.cookies.get("session");
 
   //Return to /login if don't have a session
   if (!session) {
-    return NextResponse.redirect(new URL('/signin', request.url));
+    return NextResponse.redirect(new URL("/signin", request.url));
   }
 
+  const origin = request.nextUrl.origin;
+
   //Call the authentication endpoint
-  const responseAPI = await fetch('http://localhost:3000/api/signin', {
-    method: 'GET',
+  const responseAPI = await fetch(`${origin}/api/signin`, {
+    method: "GET",
     headers: {
       Cookie: `session=${session?.value}`,
     },
   });
 
-  //Check for 404. Not sure if this is appropriate here.
-  if (responseAPI.status === 404) {
-    return NextResponse.redirect(new URL('/404', request.url));
-  }
   //Return to /login if token is not authorized
   if (responseAPI.status !== 200) {
-    return NextResponse.redirect(new URL('/signin', request.url));
+    return NextResponse.redirect(new URL("/signin", request.url));
   }
 
   // Get the signed-in user's UID from the response
   const signedInUser = await responseAPI.json();
-  // console.log('middleware: signedInUser: ', signedInUser);
   const signedInUserUid = signedInUser.decodedClaims.uid;
 
   // does admin exist on non-admin users as admin:false or is it not present?
@@ -40,23 +34,23 @@ export async function middleware(request: NextRequest, response: NextResponse) {
   const pathname = request.nextUrl.pathname;
 
   // Check if requested page contains a user ID and ensure it is the current user.
-  if (pathname.startsWith('/profile/') && !pathname.includes(signedInUserUid)) {
+  if (pathname.startsWith("/profile/") && !pathname.includes(signedInUserUid)) {
     return NextResponse.redirect(
       new URL(`/profile/${signedInUserUid}`, request.url)
     );
   }
 
   //Check if the user is an admin
-  if (pathname.startsWith('/admin') && !signedInUserAdmin) {
-    return NextResponse.redirect(new URL('/401', request.url));
+  if (pathname.startsWith("/admin") && !signedInUserAdmin) {
+    return NextResponse.redirect(new URL("/401", request.url));
   }
 
   //My account is the only super user
   if (
-    pathname.includes('superuser') &&
-    signedInUserUid !== 'dBTHm0AGMSTG5RKD6yZPkp0fGir2'
+    pathname.includes("superuser") &&
+    signedInUserUid !== process.env.NEXT_PUBLIC_SUPER_USER_UID
   ) {
-    return NextResponse.redirect(new URL('/401', request.url));
+    return NextResponse.redirect(new URL("/401", request.url));
   }
 
   return NextResponse.next();
@@ -64,5 +58,5 @@ export async function middleware(request: NextRequest, response: NextResponse) {
 
 //Add your protected routes
 export const config = {
-  matcher: ['/admin/:path*', '/dashboard', '/profile/:path*'],
+  matcher: ["/admin/:path*", "/dashboard", "/profile/:path*"],
 };
